c24ccce17c8dd862a4a14fd4b3bb38233ba70f17
Charleeslemes <charleslemes21@gmail.com>
2025-02-27 19:45:57 -0300

    alterado o arquivo functions desmembrando para 3 arquivos checkout-customizado, product-list e user-custom-menu

--- functions.php
+++ functions.php
@@ -1,102 +1,48 @@
 <?php
-
-add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
-
-// Our hooked in function - $fields is passed via the filter!
-function custom_override_checkout_fields( $fields ) {
-
-  print_r($fields);
-
-    return $fields;
-}
-
-
-
-
-
-function handel_css(){
-  wp_register_style('handel-style', get_template_directory_uri() . '/style.css', [], '1.0.0');
-  wp_enqueue_style('handel-style');
-}
-
-add_action('wp_enqueue_scripts', 'handel_css');
-
-function handel_add_woocommerce_support(){
+function handel_add_woocommerce_support() {
   add_theme_support('woocommerce');
 }
-
 add_action('after_setup_theme', 'handel_add_woocommerce_support');
 
-function handel_custom_images(){
-  add_image_size('slide', 1000, 800, ['center', 'top']);
-  update_option('medium_crop', 1 );
+function handel_css() {
+  wp_register_style('handel-style', get_template_directory_uri() . '/style.css', [], '1.0.0', false);
+  wp_enqueue_style('handel-style');
 }
+add_action('wp_enqueue_scripts', 'handel_css');
 
-add_action('after_setup_rheme', 'handel_custom_images');
+function handel_custom_images() {
+  add_image_size('slide', 1000, 800, ['center', 'top']);
+  update_option('medium_crop', 1);
+}
+add_action('after_setup_theme', 'handel_custom_images');
 
-
-function handel_loop_shop_per_page(){
+function handel_loop_shop_per_page() {
   return 6;
 }
-
 add_filter('loop_shop_per_page', 'handel_loop_shop_per_page');
 
-
-
-function remove_some_body_class($classes){
+function remove_some_body_class($classes) {
   $woo_class = array_search('woocommerce', $classes);
   $woopage_class = array_search('woocommerce-page', $classes);
-
   $search = in_array('archive', $classes) || in_array('product-template-default', $classes);
-
-  if($woo_class && $woopage_class && $search){
+  if($woo_class && $woopage_class && $search) {
     unset($classes[$woo_class]);
     unset($classes[$woopage_class]);
   }
-
   return $classes;
 }
-
 add_filter('body_class', 'remove_some_body_class');
 
+include(get_template_directory() . '/inc/product-list.php');
 
+add_filter('woocommerce_enable_order_notes_field', '__return_false');
 
-function format_products($products, $imgSize = 'medium'){
-  $products_final = [];
-  foreach($products as $product){
-      $products_final[] =[
-        'name' => $product->get_name(),
-        'price' => $product->get_price_html(),
-        'link' => $product->get_permalink(),
-        'img'=> wp_get_attachment_image_src($product->get_image_id(), $imgSize)[0],
-      ];
-  }
-  return $products_final;
+function meu_dashboard() {
+  echo 'Origamid';
 }
 
+add_action('woocommerce_account_dashboard', 'meu_dashboard');
 
-
-  function handel_product_list($products){?>
-
-    <ul class="products-list">
-            <?php foreach($products as $product) { ?>
-                <li class="product-item">
-                  <a href="<?= $product['link'] ?>">
-                    <div class="product-info">
-                      <img src="<?= $product['img'] ?>" alt="<?= $product['name'] ?>">
-                      <h2><?= $product['name'] ?> - <span><?= $product['price'] ?></span></h2>
-                    </div>
-
-                    <div class="product-overlay">
-                      <span class="btn-link">Ver Mais</span>
-                    </div>
-                  </a>
-                </li>
-            <?php } ?>
-          </ul>
-
-  <?php }?> 
-
-
-
-
+include(get_template_directory() . '/inc/user-custom-menu.php');
+include(get_template_directory() . '/inc/checkout-customizado.php');
+?>
